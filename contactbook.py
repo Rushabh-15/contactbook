a7ba31037257779040d64d8c9c9764ad6fa8ad10
rahulchavan3 <rahulchavan3030@gmail.com>
2023-10-11 18:35:20 +0530

    added validation

--- contactbook.py
+++ contactbook.py
@@ -1,7 +1,8 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
-from tkinter.messagebox import *
+from tkinter.messagebox import showinfo, showwarning, showerror
 import csv
+import re
 
 class ContactBookApp:
     def __init__(self, root):
@@ -16,8 +17,8 @@ class ContactBookApp:
         self.root.bind("<Control-f>", self.focus_search_entry)  # Ctrl + F to focus on the search entry
 
         self.notebook = ttk.Notebook(self.root)
-        self.tab1 = Frame(self.notebook, bg="darkturquoise")
-        self.tab2 = Frame(self.notebook, bg="darkturquoise")
+        self.tab1 = tk.Frame(self.notebook, bg="darkturquoise")
+        self.tab2 = tk.Frame(self.notebook, bg="darkturquoise")
         self.notebook.add(self.tab1, text="Add Contact")
         self.notebook.add(self.tab2, text="View Contacts")
         self.notebook.pack()
@@ -26,42 +27,42 @@ class ContactBookApp:
         self.init_view_contacts_tab()
 
     def init_add_contact_tab(self):
-        label = Label(self.tab1, text="Add Contact", font=("Arial", 20), bg="#FF9912")
+        label = tk.Label(self.tab1, text="Add Contact", font=("Arial", 20), bg="#FF9912")
         label.pack(pady=10)
 
-        name_label = Label(self.tab1, text="Name:", font=("Arial", 15), bg="lawngreen")
+        name_label = tk.Label(self.tab1, text="Name:", font=("Arial", 15), bg="lawngreen")
         name_label.pack()
-        self.name_entry = Entry(self.tab1, font=("Arial", 15))
+        self.name_entry = tk.Entry(self.tab1, font=("Arial", 15))
         self.name_entry.pack(pady=10)
 
-        phone_label = Label(self.tab1, text="Phone:", font=("Arial", 15), bg="lawngreen")
+        phone_label = tk.Label(self.tab1, text="Phone:", font=("Arial", 15), bg="lawngreen")
         phone_label.pack()
-        self.phone_entry = Entry(self.tab1, font=("Arial", 15))
+        self.phone_entry = tk.Entry(self.tab1, font=("Arial", 15))
         self.phone_entry.pack(pady=10)
 
-        email_label = Label(self.tab1, text="Email:", font=("Arial", 15), bg="lawngreen")
+        email_label = tk.Label(self.tab1, text="Email:", font=("Arial", 15), bg="lawngreen")
         email_label.pack()
-        self.email_entry = Entry(self.tab1, font=("Arial", 15))
+        self.email_entry = tk.Entry(self.tab1, font=("Arial", 15))
         self.email_entry.pack(pady=10)
 
-        address_label = Label(self.tab1, text="Address:", font=("Arial", 15), bg="lawngreen")
+        address_label = tk.Label(self.tab1, text="Address:", font=("Arial", 15), bg="lawngreen")
         address_label.pack()
-        self.address_entry = Entry(self.tab1, font=("Arial", 15))
+        self.address_entry = tk.Entry(self.tab1, font=("Arial", 15))
         self.address_entry.pack(pady=10)
 
-        add_button = Button(self.tab1, text="Add Contact", command=self.add_contact, font=("Arial", 15), bg="lawngreen")
+        add_button = tk.Button(self.tab1, text="Add Contact", command=self.add_contact, font=("Arial", 15), bg="lawngreen")
         add_button.pack(pady=10)
 
     def init_view_contacts_tab(self):
-        label = Label(self.tab2, text="View Contacts", font=("Arial", 20), bg="#FF9912")
+        label = tk.Label(self.tab2, text="View Contacts", font=("Arial", 20), bg="#FF9912")
         label.pack(pady=10)
 
-        search_label = Label(self.tab2, text="Search:", font=("Arial", 15), bg="lawngreen")
+        search_label = tk.Label(self.tab2, text="Search:", font=("Arial", 15), bg="lawngreen")
         search_label.pack()
-        self.search_entry = Entry(self.tab2, font=("Arial", 15))
+        self.search_entry = tk.Entry(self.tab2, font=("Arial", 15))
         self.search_entry.pack(pady=10)
 
-        search_button = Button(self.tab2, text="Search", command=self.search_contacts, font=("Arial", 15), bg="lawngreen")
+        search_button = tk.Button(self.tab2, text="Search", command=self.search_contacts, font=("Arial", 15), bg="lawngreen")
         search_button.pack(pady=5)
 
         self.contacts_treeview = ttk.Treeview(self.tab2, columns=("Name", "Phone", "Email", "Address"), show="headings")
@@ -72,30 +73,29 @@ class ContactBookApp:
         self.contacts_treeview.pack(padx=10, pady=10)
         self.update_contacts_treeview()
 
-        button_frame = Frame(self.tab2, bg="darkturquoise")
+        button_frame = tk.Frame(self.tab2, bg="darkturquoise")
         button_frame.pack(pady=10)
 
-        edit_button = Button(button_frame, text="Edit Contact", command=self.edit_contact, font=("Arial", 15), bg="lawngreen")
-        edit_button.pack(side=LEFT, padx=10)
+        edit_button = tk.Button(button_frame, text="Edit Contact", command=self.edit_contact, font=("Arial", 15), bg="lawngreen")
+        edit_button.pack(side=tk.LEFT, padx=10)
 
-        delete_button = Button(button_frame, text="Delete Contact", command=self.delete_contact, font=("Arial", 15), bg="lawngreen")
-        delete_button.pack(side=LEFT, padx=10)
+        delete_button = tk.Button(button_frame, text="Delete Contact", command=self.delete_contact, font=("Arial", 15), bg="lawngreen")
+        delete_button.pack(side=tk.LEFT, padx=10)
 
-        merge_button = Button(button_frame, text="Merge Contacts", command=self.merge_contacts, font=("Arial", 15), bg="lawngreen")
-        merge_button.pack(side=LEFT, padx=10)
+        merge_button = tk.Button(button_frame, text="Merge Contacts", command=self.merge_contacts, font=("Arial", 15), bg="lawngreen")
+        merge_button.pack(side=tk.LEFT, padx=10)
 
-        export_button = Button(button_frame, text="Export Contacts (CSV)", command=self.export_contacts_to_csv, font=("Arial", 15), bg="lawngreen")
-        export_button.pack(side=LEFT, padx=10)
+        export_button = tk.Button(button_frame, text="Export Contacts (CSV)", command=self.export_contacts_to_csv, font=("Arial", 15), bg="lawngreen")
+        export_button.pack(side=tk.LEFT, padx=10)
 
-        load_button = Button(button_frame, text="Load Contacts", command=self.load_contacts_from_csv, font=("Arial", 15), bg="lawngreen")
-        load_button.pack(side=LEFT, padx=10)
+        load_button = tk.Button(button_frame, text="Load Contacts", command=self.load_contacts_from_csv, font=("Arial", 15), bg="lawngreen")
+        load_button.pack(side=tk.LEFT, padx=10)
 
-        stats_button = Button(button_frame, text="Contact Statistics", command=self.display_contact_statistics,
+        stats_button = tk.Button(button_frame, text="Contact Statistics", command=self.display_contact_statistics,
                               font=("Arial", 15), bg="lawngreen")
-        stats_button.pack(side=LEFT, padx=10)
+        stats_button.pack(side=tk.LEFT, padx=10)
 
-        # Label to display contact statistics
-        self.stats_label = Label(self.tab2, text="", font=("Arial", 15), bg="darkturquoise")
+        self.stats_label = tk.Label(self.tab2, text="", font=("Arial", 15), bg="darkturquoise")
         self.stats_label.pack(pady=5)
 
     def display_contact_statistics(self):
@@ -109,19 +109,25 @@ class ContactBookApp:
         email = self.email_entry.get()
         address = self.address_entry.get()
 
-        if name and phone:
+        if not name:
+            showwarning("Validation Error", "Name is required.")
+        elif not phone:
+            showwarning("Validation Error", "Phone is required.")
+        elif not self.is_valid_phone(phone):
+            showwarning("Validation Error", "Phone number should only contain digits.")
+        elif email and not self.is_valid_email(email):
+            showwarning("Validation Error", "Invalid Email format.")
+        else:
             self.contacts.append({"Name": name, "Phone": phone, "Email": email, "Address": address})
             self.clear_add_contact_fields()
             self.update_contacts_treeview()
             showinfo("Success", "Contact added successfully!")
-        else:
-            showwarning("Error", "Name and Phone are required!")
 
     def clear_add_contact_fields(self):
-        self.name_entry.delete(0, END)
-        self.phone_entry.delete(0, END)
-        self.email_entry.delete(0, END)
-        self.address_entry.delete(0, END)
+        self.name_entry.delete(0, tk.END)
+        self.phone_entry.delete(0, tk.END)
+        self.email_entry.delete(0, tk.END)
+        self.address_entry.delete(0, tk.END)
 
     def update_contacts_treeview(self):
         self.contacts.sort(key=lambda contact: contact["Name"].lower())
@@ -147,32 +153,32 @@ class ContactBookApp:
         selected_item = self.contacts_treeview.selection()
         if selected_item:
             selected_contact = self.contacts[self.contacts_treeview.index(selected_item)]
-            edit_window = Toplevel(self.root)
+            edit_window = tk.Toplevel(self.root)
             edit_window.title("Edit Contact")
             edit_window.geometry("300x300")
             edit_window.config(bg="darkturquoise")
 
-            edit_name_label = Label(edit_window, text="Name:", font=("Arial", 15), bg="lawngreen")
+            edit_name_label = tk.Label(edit_window, text="Name:", font=("Arial", 15), bg="lawngreen")
             edit_name_label.pack()
-            edit_name_entry = Entry(edit_window, font=("Arial", 15))
+            edit_name_entry = tk.Entry(edit_window, font=("Arial", 15))
             edit_name_entry.pack()
             edit_name_entry.insert(0, selected_contact["Name"])
 
-            edit_phone_label = Label(edit_window, text="Phone:", font=("Arial", 15), bg="lawngreen")
+            edit_phone_label = tk.Label(edit_window, text="Phone:", font=("Arial", 15), bg="lawngreen")
             edit_phone_label.pack()
-            edit_phone_entry = Entry(edit_window, font=("Arial", 15))
+            edit_phone_entry = tk.Entry(edit_window, font=("Arial", 15))
             edit_phone_entry.pack()
             edit_phone_entry.insert(0, selected_contact["Phone"])
 
-            edit_email_label = Label(edit_window, text="Email:", font=("Arial", 15), bg="lawngreen")
+            edit_email_label = tk.Label(edit_window, text="Email:", font=("Arial", 15), bg="lawngreen")
             edit_email_label.pack()
-            edit_email_entry = Entry(edit_window, font=("Arial", 15))
+            edit_email_entry = tk.Entry(edit_window, font=("Arial", 15))
             edit_email_entry.pack()
             edit_email_entry.insert(0, selected_contact["Email"])
 
-            edit_address_label = Label(edit_window, text="Address:", font=("Arial", 15), bg="lawngreen")
+            edit_address_label = tk.Label(edit_window, text="Address:", font=("Arial", 15), bg="lawngreen")
             edit_address_label.pack()
-            edit_address_entry = Entry(edit_window, font=("Arial", 15))
+            edit_address_entry = tk.Entry(edit_window, font=("Arial", 15))
             edit_address_entry.pack()
             edit_address_entry.insert(0, selected_contact["Address"])
 
@@ -184,7 +190,7 @@ class ContactBookApp:
                 self.update_contacts_treeview()
                 edit_window.destroy()
 
-            save_button = Button(edit_window, text="Save", command=save_edited_contact, font=("Arial", 15), bg="lawngreen")
+            save_button = tk.Button(edit_window, text="Save", command=save_edited_contact, font=("Arial", 15), bg="lawngreen")
             save_button.pack(pady=10)
 
     def delete_contact(self, event=None):
@@ -233,7 +239,15 @@ class ContactBookApp:
         except Exception as e:
             showerror("Error", f"An error occurred: {str(e)}")
 
+    def is_valid_phone(self, phone):
+        phone_pattern = r'^\d+$'  # Regular expression for digits only
+        return re.match(phone_pattern, phone) is not None
+
+    def is_valid_email(self, email):
+        email_pattern = r'^\S+@\S+\.\S+'
+        return re.match(email_pattern, email) is not None
+
 if __name__ == "__main__":
-    root = Tk()
+    root = tk.Tk()
     app = ContactBookApp(root)
     root.mainloop()
